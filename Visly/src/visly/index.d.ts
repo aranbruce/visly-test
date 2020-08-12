e48e57c51e1d612b78fa12ca466bbfa5aaf06b71
Aran Bruce-Caddick <aran@koodoo.io>
2020-08-12 16:10:06 +0100

    Initialize project using Create React App

---
+++ Visly/src/visly/index.d.ts
@@ -0,0 +1,6 @@
+import * as colors from './colors';
+import * as textstyles from './textstyles';
+import * as icons from './icons';
+import * as __refs__ from './__refs__';
+export { icons, textstyles, colors, __refs__ };
+export * from './components';
