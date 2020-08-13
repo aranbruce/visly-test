6a53fe808142451c773ebd0f3b883bf865347478
Aran Bruce-Caddick <aran@koodoo.io>
2020-08-13 12:35:08 +0100

    Updates

---
+++ precache-manifest.4869eebea1157dfc7863787b032cd257.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be35bd62e6fdad154395a209ceff6ef",
+    "url": "/visly-test/index.html"
+  },
+  {
+    "revision": "2886efa86abdb5406286",
+    "url": "/visly-test/static/css/main.0b8c4927.chunk.css"
+  },
+  {
+    "revision": "3b0ac24304893fc3ca1c",
+    "url": "/visly-test/static/js/2.7f792c81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visly-test/static/js/2.7f792c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2886efa86abdb5406286",
+    "url": "/visly-test/static/js/main.687638e3.chunk.js"
+  },
+  {
+    "revision": "deefe4b260da1e96fff3",
+    "url": "/visly-test/static/js/runtime-main.97a9539e.js"
+  },
+  {
+    "revision": "34dafd9e8d99393a4225c47914bb1e70",
+    "url": "/visly-test/static/media/22ef53ca-3265-49f9-85f5-c00ff038c977@1x.34dafd9e.svg"
+  },
+  {
+    "revision": "9dbfa1af0260596a0745cd5733aaf8cb",
+    "url": "/visly-test/static/media/335f3298-73c2-4b49-87f0-bdd5380a4035@1x.9dbfa1af.svg"
+  },
+  {
+    "revision": "10a5b071548379871a331ddae24e289f",
+    "url": "/visly-test/static/media/5cd79169-4d72-486a-bd71-20cfa0420bb2@1x.10a5b071.svg"
+  },
+  {
+    "revision": "0873edbac60f197bbaef1ab9f7d32d19",
+    "url": "/visly-test/static/media/7900b30b-3148-4379-bb23-489f4b637df5@1x.0873edba.svg"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/visly-test/static/media/925b1f14-e32f-4af1-b2ee-80132fd73b20.58aef543.ttf"
+  },
+  {
+    "revision": "2fdb1cc1799dc39bcc07d6268b126028",
+    "url": "/visly-test/static/media/c0d36d31-1367-4c7a-91be-c1ac14e5c3c5@1x.2fdb1cc1.svg"
+  },
+  {
+    "revision": "6acd960f05cb6727b02f3b78598318cf",
+    "url": "/visly-test/static/media/c6f11592-eb2a-4697-9b7c-6c7c31b99f89@1x.6acd960f.svg"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/visly-test/static/media/c775fc02-e85e-4e5f-b67a-b62e4ed19fb9.11eabca2.ttf"
+  },
+  {
+    "revision": "289f0dc4f5815ad6fbd70f396e19b334",
+    "url": "/visly-test/static/media/c960c1cc-f05b-4842-972b-c497154e2e5b@1x.289f0dc4.svg"
+  },
+  {
+    "revision": "343a2b9fba70e308ac39211592f96d7a",
+    "url": "/visly-test/static/media/d0171f72-9dbe-4e54-b469-a4432d2beca6@1x.343a2b9f.svg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/visly-test/static/media/d7d2c5b4-4c7b-4efb-9c21-0ef70f800be8.e07df86c.ttf"
+  },
+  {
+    "revision": "c4f5bcd03d6f8fc2dd259386a11b8d0e",
+    "url": "/visly-test/static/media/fd204fbe-52a7-4ca8-becd-8a04ae285d64@1x.c4f5bcd0.svg"
+  }
+]);
